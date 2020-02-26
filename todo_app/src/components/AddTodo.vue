917e97ad50592c20ae3813123729361310d417ae
szczepan27 <s.szymon1127@gmail.com>
2020-02-26 20:31:21 +0100

    simple todos layout

--- todo_app/src/components/AddTodo.vue
+++ todo_app/src/components/AddTodo.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="add-todo">
-        hola here we addin
+        <label for="dzialanie">Działanie: </label><input type="text" name="dzialanie" id="dzialanie">
+        <label for="odpowiedzialny">Odpowiedzialny: </label><input type="text" name="odpowiedzialny" id="odpowiedzialny">
+        <label for="data">Data wykonania: </label><input type="text" name="data" id="data">
+        <label for="stan">Stan wykonania: </label><input type="text" name="stan" id="stan">
     </div>
 </template>
 
--- todo_app/src/components/Todos.vue
+++ todo_app/src/components/Todos.vue
@@ -1,15 +1,99 @@
 <template>
     <div id="todos">
-        here we have todos yay
+        <div class="container">
+            <h2 class="todo-header">TODO LIST</h2>
+            <ul class="todo-list">
+                <li class="todo-list-header">
+                    <div>L.p.</div>
+                    <div>Działanie</div>
+                    <div>Odpowiedzialny</div>
+                    <div>Data wykonania</div>
+                    <div>Stan wykonania</div>
+                </li>
+                <li class="todo-list-item" v-for="todo in todos" :key="todo">
+                    <div>{{ todo.num }}</div>
+                    <div>{{ todo.dzial }}</div>
+                    <div>{{ todo.odp }}</div>
+                    <div>{{ todo.data }}</div>
+                    <div>{{ todo.stan }}</div>
+                </li>
+            </ul>
+            <button class="go-add" @click="goToAdd()">Add a todo</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            todos: [
+                {
+                num: '1',
+                dzial: 'hehs',
+                odp: 'kowalski',
+                data: '27.11.21',
+                stan: 'X'
+                }
+            ]
+        }
+    },
+    methods: {
+        goToAdd(){
+            this.$router.push({name: 'AddTodo'})
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+*{
+    font-family: sans-serif;
+}
+.container{
+    width: 60%;
+    margin: auto;
+    margin-top: 5%;
+    display: grid;
+}
 
+.todo-header{
+    margin-bottom: 3em;
+    text-align: center;
+}
+
+.todo-list{
+    list-style: none;
+    display: block;
+    width: 100%;
+}
+
+li{
+    display: block;
+    display: flex;
+}
+li>*{
+    flex-basis: 22.5%;
+    padding: 1em;
+    text-align: center;
+    border: 1px solid #57ACC9;
+}
+li>*:nth-child(1){
+    flex-basis: 10%;
+}
+
+.todo-list-header{
+    font-weight: bold;
+}
+
+.go-add{
+    padding: 1em;
+    border: none;
+    border: 1px solid #57ACC9;
+    border-radius: .5em;
+    color: #57ACC9;
+    background-color: #eee;
+    margin-top: 1em;
+    justify-self: end;
+}
 </style>
